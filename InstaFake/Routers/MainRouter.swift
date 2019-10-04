faff56067e7ec3a7a66aa82aa5f2d47fe27cedfb
Artem <artem_grebinik@icloud.com>
2019-10-04 12:35:04 +0300

    Add MainRouter file

---
+++ InstaFake/Routers/MainRouter.swift
@@ -0,0 +1,17 @@
+//
+//  MainRouter.swift
+//  InstaFake
+//
+//  Created by Artem Grebinik on 10/4/19.
+//  Copyright © 2019 Artem Grebinik. All rights reserved.
+//
+
+import UIKit
+
+final class MainRouter {
+    func root(_ window: inout UIWindow?) {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+    }
+}
